f829e3f784878616228f3da81928ee45216195f2
sfabig <fabig@th-brandenburg.de>
2018-01-21 22:43:16 +0100

    Tests

--- src/unittest/python/helloworld_tests.py
+++ src/unittest/python/helloworld_tests.py
@@ -7,7 +7,9 @@ from src.main.python.helloworld import helloworld
 class HelloWorldTest(unittest.TestCase):
     def test_should_issue_hello_world_message(self):
         out = mock()
-
         helloworld(out)
-
         verify(out).write("Hello world of Python\n")
+
+
+if __name__ == '__main__':
+    unittest.main()
