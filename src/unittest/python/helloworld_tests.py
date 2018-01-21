42b2d064caedd145d7b7014030b557557882b65c
sfabig <fabig@th-brandenburg.de>
2018-01-21 22:14:56 +0100

    Aufräumen

--- src/unittest/python/helloworld_tests.py
+++ src/unittest/python/helloworld_tests.py
@@ -1,7 +1,8 @@
 from mockito import mock, verify
 import unittest
 
-from helloworld import helloworld
+from src.main.python.helloworld import helloworld
+
 
 class HelloWorldTest(unittest.TestCase):
     def test_should_issue_hello_world_message(self):
