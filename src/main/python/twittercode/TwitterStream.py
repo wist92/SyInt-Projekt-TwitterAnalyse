f68c99fb82855c740cb08cb7d9fb06cdb157d40d
romeolandry <romeo_kamgo@yahoo.com>
2018-01-23 14:10:16 +0100

    add source src

---
+++ src/main/python/twittercode/TwitterStream.py
@@ -0,0 +1,58 @@
+# Import the necessary methods from tweepy library
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
+import time
+
+# Variables that contains the user credentials to access Twitter API
+access_token = ""
+access_token_secret = ""
+consumer_key = ""
+consumer_secret = ""
+
+# This is a basic listener that just prints received tweets to stdout.
+class StdOutListener(StreamListener):
+    def __init__(self, time_limit=10, file_name='twitter_data.txt'):
+        self.start_time = time.time()
+        self.limit = time_limit
+        self.saveFile = open(file_name, 'w+')
+        super(StdOutListener, self).__init__()
+
+    def on_data(self, data):
+        print (data)
+        if (time.time() - self.start_time) < self.limit:
+            if "{\"delete\":" not in data:
+                self.saveFile.write(data)
+            return True
+        else:
+            self.saveFile.close()
+            return False
+
+    def on_error(self, status):
+        print (status)
+        if status == 420:
+            # returning False in on_data disconnects the stream
+            return False
+
+
+def twitter_sampling(time, name):
+    # This handles Twitter authentication and the connection to Twitter Streaming API
+    stream_listener = StdOutListener(time_limit=time, file_name='twitter_data_' + name + '.txt')
+    auth = OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    stream = Stream(auth, stream_listener)
+
+    # This line filter Twitter Streams to capture data by the keywords: 'python', 'javascript', 'ruby'
+    stream.sample()
+
+
+if __name__ == '__main__':
+
+    # This handles Twitter authentication and the connection to Twitter Streaming API
+    stream_listener = StdOutListener(time_limit=60)
+    auth = OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    stream = Stream(auth, stream_listener)
+
+    # This line filter Twitter Streams to capture data by the keywords: 'python', 'javascript', 'ruby'
+    stream.sample()
