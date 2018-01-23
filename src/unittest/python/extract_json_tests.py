ffeeec35971732c8b32a7eb616f0f10d12127ca4
Your Name <you@example.com>
2018-01-23 13:43:06 +0100

    PyBuilder Test 2

--- src/unittest/python/extract_json_tests.py
+++ src/unittest/python/extract_json_tests.py
@@ -8,14 +8,14 @@ from twittercode.extract_json import import_twitter_data, create_plot
 
 class TestJsonExtracting(unittest.TestCase):
     def test_reading_twitter_json_file_returns_non_empty_array(self):
-        self.assertTrue(import_twitter_data('twitter_data.txt') is not None)
+        self.assertTrue(import_twitter_data('./twitter_data.txt') is not None)
 
     def test_plot_creation_should_be_successful(self):
         test = pandas.DataFrame()
         test['hashtag'] = ['hallo', 'u', 'suck']
         c = test['hashtag'].value_counts()
         create_plot(c, 'Test', 'hallo_u_suck')
-        self.assertTrue(os.path.exists('hallo_u_suck_top_Test.png'))
+        self.assertTrue(os.path.exists('./hallo_u_suck_top_Test.png'))
 
 
 if __name__ == '__main__':
