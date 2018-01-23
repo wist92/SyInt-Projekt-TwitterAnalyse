92d1c637fde0848e6970f36440f03ec527833cb2
sfabig <fabig@th-brandenburg.de>
2018-01-23 12:43:41 +0100

    Very easy Exception Handling

--- src/main/python/bottle_twitter.py
+++ src/main/python/bottle_twitter.py
@@ -1,7 +1,7 @@
 import string
 import random
 import datetime
-
+import traceback
 from bottle import template, route, request, run
 
 from src.main.python.TwitterStream import twitter_sampling
@@ -16,19 +16,26 @@ def twitter():
     result_json_name = now + '_' + (''.join(random.choice(string.ascii_lowercase +
                                                           string.ascii_uppercase +
                                                           string.digits) for i in range(5)))
-    print("Reading TwitterSampleStream...")
-    twitter_sampling(time=sampling_time, name=result_json_name)
-    print("Extracting Data from JSON...")
-    result_file_links = extract_data(path_to_json_file=result_json_name)
-    print("Deleting unneeded files...")
-    delete_txt_and_png()
+    try:
+        print("Reading TwitterSampleStream...")
+        twitter_sampling(time=sampling_time, name=result_json_name)
+        print("Extracting Data from JSON...")
+        result_file_links = extract_data(path_to_json_file=result_json_name)
+        print("Deleting unneeded files...")
+        delete_txt_and_png()
 
-    info = {'country_result_link': result_file_links[1],
-            'hashtag_result_link': result_file_links[2],
-            'language_result_link': result_file_links[0]
-            }
+        info = {'country_result_link': result_file_links[1],
+                'hashtag_result_link': result_file_links[2],
+                'language_result_link': result_file_links[0]
+                }
 
-    return template('result_vorlage.tpl', info)
+        return template('result_vorlage.tpl', info)
+    except UnicodeEncodeError as e:
+        delete_txt_and_png()
+        return 'Most found Hashtag couldn\'t be written into database.'
+    except TypeError as e:
+        delete_txt_and_png()
+        return 'Analysis time was too short to find hashtags, countries or languages in TwitterSampleStream.'
 
 
 run(host='127.0.0.1', port=80)
